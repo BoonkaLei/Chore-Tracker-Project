897340d86ea101e61edbfdb25c4feb11e4706045
BoonkaLei <vbergendale95@outlook.com>
2025-10-05 23:42:49 -0400

    Update index.js

--- api/GetChores/index.js
+++ api/GetChores/index.js
@@ -1,1 +1,11 @@
-
+module.exports = async function (context, req) {
+context.log('Fetching chores...');
+const chores = [
+{ id: 1, name: 'Make bed', status: 'Incomplete' },
+{ id: 2, name: 'Feed cat', status: 'Complete' }
+];
+context.res = {
+status: 200,
+body: chores
+};
+};
